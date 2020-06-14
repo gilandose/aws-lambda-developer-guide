2b5cc852ad3748bec2d6ddfce6075aa6631108cd
richardgilmore <richard.gilmore@slicelife.com>
2020-06-14 01:14:54 +0100

    made some beautiful changes

--- sample-apps/java-events/src/main/java/example/HandlerApiGateway.java
+++ sample-apps/java-events/src/main/java/example/HandlerApiGateway.java
@@ -2,19 +2,12 @@ package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.util.Map;
 import java.util.HashMap;
 
 // Handler value: example.HandlerApiGateway
@@ -27,23 +20,14 @@ public class HandlerApiGateway implements RequestHandler<APIGatewayProxyRequestE
     var headers = new HashMap<String,String>();
     headers.put("Content-Type", "application/json");
     headers.put("X-Custom-Header", "application/json");
-    var client = HttpClient.newHttpClient();
-    var request = HttpRequest.newBuilder()
-            .GET()
-            .version(HttpClient.Version.HTTP_2)
-            .uri(URI.create("https://checkip.amazonaws.com"))
-            .timeout(Duration.ofSeconds(15))
-            .build();
     var logger = context.getLogger();
-
     try {
-      var fresponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-      String output = String.format("{ \"message\": \"hello world yeet from %s\", \"location\": \"%s\" }",yeet.getYeet(), fresponse.body());
+      String output = String.format("{ \"message\": \"hello world yeet from %s\" }",yeet.getYeet());
       Util.logEnvironment(event, context, gson);
       return new APIGatewayProxyResponseEvent()
               .withBody(output)
               .withHeaders(headers)
-              .withStatusCode(fresponse.statusCode())
+              .withStatusCode(200)
               .withIsBase64Encoded(false);
     } catch (Exception e) {
       return new APIGatewayProxyResponseEvent()
